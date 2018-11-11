a76177f4aa85c457e10b8066a2d05ec29ca38525
Huyf1995 <spyssor@gmail.com>
2018-11-11 18:43:25 +0800

    完成影片模块

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFilmActorTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocFilmActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影片与演员映射表 Mapper 接口
+ * </p>
+ *
+ * @author huyufan
+ * @since 2018-11-11
+ */
+public interface MoocFilmActorTMapper extends BaseMapper<MoocFilmActorT> {
+
+}
---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocYearDictTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocYearDictT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 年代信息表 Mapper 接口
+ * </p>
+ *
+ * @author huyufan
+ * @since 2018-11-10
+ */
+public interface MoocYearDictTMapper extends BaseMapper<MoocYearDictT> {
+
+}
